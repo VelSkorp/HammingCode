06321eaba2a417441219989399b98e9531e2b208
Vlad Kontsevich <myEmail@mail.ru>
2020-02-03 11:23:05 +0300

    Добавьте файлы проекта.

---
+++ HammingCode/Decoder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HammingCode
+{
+    class Decoder
+    {
+        public Decoder(string BinMsgContrBit, string ContrValues)
+        {
+            Console.WriteLine();
+            Console.WriteLine(BinMsgContrBit);
+
+            Coder coder = new Coder();
+            coder.CreateMatrixOfX(BinMsgContrBit);
+            string Values = coder.GetContrValues;
+            Console.WriteLine(Values);
+
+            if (Values == ContrValues)
+                Console.WriteLine("ошибок нет");
+            else
+                BinMsgContrBit=FindFixError(BinMsgContrBit, ContrValues, Values);
+
+            BinToChar(BinMsgContrBit);
+        }
+
+        private static string FindFixError(string BinMsgContrBit, string ContrValues, string Values)
+        {
+            int ErrorBit=0;
+            for (int i = 0; i < ContrValues.Length; i++)
+            {
+                if (ContrValues[i] != Values[i])
+                    ErrorBit += (int)Math.Pow(2, i);
+            }
+            Console.WriteLine("ошибка в: {0} бите",ErrorBit);
+            if (BinMsgContrBit[ErrorBit-1]=='1')
+                BinMsgContrBit = BinMsgContrBit.Remove(ErrorBit-1, 1).Insert(ErrorBit-1, "0");
+            else
+                BinMsgContrBit = BinMsgContrBit.Remove(ErrorBit-1, 1).Insert(ErrorBit-1, "1");
+            Console.WriteLine(BinMsgContrBit);
+            return BinMsgContrBit;
+        }
+
+        private static void BinToChar(string BinMsgContrBit)
+        {
+            string Message="";
+            int Pow = 0;
+            for (int i = 0; i < BinMsgContrBit.Length; i++)
+                if (i!=Math.Pow(2,Pow)-1)
+                    Message += BinMsgContrBit[i];
+                else
+                    Pow++;
+
+            List<byte> ByteList = new List<byte>();
+
+            for (int i = 0; i < Message.Length; i+=8)
+            {
+                ByteList.Add(Convert.ToByte(Message.Substring(i, 8), 2));
+            }
+
+            Message = Encoding.UTF8.GetString(ByteList.ToArray());
+            Console.WriteLine(Message);
+        }
+
+
+    }
+}
---
+++ HammingCode/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace HammingCode
+{
+    class Program
+    {
+        private static string Message;
+        private static string BinaryErrorMessage;
+        private static string BinaryMessage;
+        private static Coder coder;
+        private static Decoder decoder;
+
+        static void Main()
+        {
+            Console.WriteLine("Введите сообщение");
+            Message = Console.ReadLine();
+            StringToBinary(Message);
+            coder = new Coder(BinaryMessage);
+
+            SendErrorMessage();
+
+            Console.ReadKey();
+        }
+
+        private static void StringToBinary(string message)
+        {
+            byte[] MessageCode = Encoding.UTF8.GetBytes(Message);
+            for (int i = 0; i < MessageCode.Length; i++)
+                BinaryMessage += Convert.ToString(MessageCode[i], 2).PadLeft(8,'0');
+        }
+
+        private static void SendErrorMessage()
+        {
+            BinaryErrorMessage = coder.GetMessage;
+            Console.WriteLine();
+            Console.WriteLine("В каком бите ошибка (none - без ошибки)");
+            string Error = Console.ReadLine();
+            if (Error == "none")
+                decoder = new Decoder(BinaryErrorMessage, coder.GetContrValues);
+            else
+            {
+                int index = Convert.ToInt32(Error);
+                if (BinaryErrorMessage[index] == '0')
+                {
+                    BinaryErrorMessage = BinaryErrorMessage.Remove(index - 1, 1).Insert(index - 1, "1");
+                    decoder = new Decoder(BinaryErrorMessage, coder.GetContrValues);
+                }
+                else if (BinaryErrorMessage[index] == '1')
+                {
+                    BinaryErrorMessage = BinaryErrorMessage.Remove(index - 1, 1).Insert(index - 1, "0");
+                    decoder = new Decoder(BinaryErrorMessage, coder.GetContrValues);
+                }
+                else
+                    Console.WriteLine("Неверно введено значение");
+            }
+        }
+
+    }
+}
